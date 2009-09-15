dabc180c6108e243d9643b376a162b82c2146816
eduardo.velloso <eduardo.velloso@5cb9f3be-97d3-11de-a1b1-8f0f9e247dbb>
2009-09-15 01:59:51 +0000

    Resolvido o problema da exibir. Antes, havia a atribuição pCorr = pArvore->pNoRaiz, e quando a arvore era vazia ele estava referenciando NULL. Bastou trocar de lugar. Também identifiquei um errinho no teste se a arvore estava vazia, mas ja corrigi.

--- trunk/simples/TestaTudo.bat
+++ trunk/simples/TestaTudo.bat
@@ -3,7 +3,5 @@ REM executa os testes dos exemplos simples
 del estatisticas.estat
 
 exemploSimples      /sTesteBasico        /lTesteBasico        /aestatisticas
-
 exemploSimples      /sTesteAvancado        /lTesteAvancado        /aestatisticas
-
 exbestat /eestatisticas
